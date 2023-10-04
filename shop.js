1456c7469bf7a54eb978bb3e2c2b3fd59181bf45
KORAMUTLA SAI RAM <144813100+sai8koramutla@users.noreply.github.com>
2023-10-04 18:19:32 +0530

    Update shop.js

--- shop.js
+++ shop.js
@@ -14,7 +14,7 @@
     var hr=date.getHours();
     var min=date.getMinutes();
     var sec=date.getSeconds();
-    a.innerText=`Đąŧë🗓️${year}/${mon+1}/${day} &nbsp&nbsp  Ŧîmê⌛${hr}:${min}:${sec}`
+    a.innerText=Date🗓️: ${year}/${mon+1}/${day} &nbsp  Time⌛:${hr}:${min}:${sec}`
     
     } ,1000)}
 
